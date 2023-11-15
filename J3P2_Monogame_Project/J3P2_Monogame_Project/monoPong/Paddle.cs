46f4b44d362d0225c8793e40e9d1e802044551f6
Hiddio <simon.verhoeff205@gmail.com>
2023-11-15 15:04:11 +0100

    Added Paddle Construction + pGameTime in GameObject

--- J3P2_Monogame_Project/J3P2_Monogame_Project/monoPong/Paddle.cs
+++ J3P2_Monogame_Project/J3P2_Monogame_Project/monoPong/Paddle.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace J3P2_Monogame_Project.monoPong
 {
-    internal class Paddle
+    class Paddle : Framework.GameObject
     {
+
+        public Paddle(Vector2 pos, float scale, GraphicsDevice pGraphicsDevice) : base(pos, scale, pGraphicsDevice)
+        {
+
+        }
     }
 }
