8ef8e34506ef01df3850d30d594ec2989f4c6502
Hiddio <simon.verhoeff205@gmail.com>
2023-11-20 10:50:09 +0100

    Added Multiple Paddles + Movement paddles

--- J3P2_Monogame_Project/J3P2_Monogame_Project/monoPong/Paddle.cs
+++ J3P2_Monogame_Project/J3P2_Monogame_Project/monoPong/Paddle.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SharpDX.XInput;
 using System;
-using System.Diagnostics;
-using System.Xml;
 
 namespace J3P2_Monogame_Project.monoPong
 {
@@ -11,15 +10,16 @@ namespace J3P2_Monogame_Project.monoPong
     {
         GameTime _gameTime;
         Vector2 _direction;
-        public Paddle(Vector2 pPosition, float pScale, Texture2D pTexture) : base(pPosition, pScale, pTexture)
+        int _paddleNumber;
+        public Paddle(Vector2 pPosition, float pScale, Texture2D pTexture, int pPaddleNumber) : base(pPosition, pScale, pTexture)
         {
-
+            _paddleNumber = pPaddleNumber;
         }
 
         public override void Update(GameTime pGameTime)
         {
             _gameTime = pGameTime;
-            MovementInput();
+            MovementInput(_paddleNumber);
         }
 
         void Movement(Vector2 pDirection, float pSpeed)
@@ -28,25 +28,64 @@ namespace J3P2_Monogame_Project.monoPong
             {
                 pDirection.Normalize();
             }
-            Console.WriteLine(pDirection);
             _position += pDirection * (float)_gameTime.ElapsedGameTime.TotalSeconds * pSpeed;
         }
 
-        void MovementInput()
+        void MovementInput(int paddleNum)
         {
             KeyboardState keyState = Keyboard.GetState();
+            GamePadState padOneState = GamePad.GetState(PlayerIndex.One);
+            GamePadState padTwoState = GamePad.GetState(PlayerIndex.Two);
             _direction.Y = 0;
 
-            if (keyState.IsKeyDown(Keys.W))
-            { 
-                _direction.Y--;
-            }
-            if (keyState.IsKeyDown(Keys.S))
+            
+            switch (paddleNum)
             {
-                _direction.Y++;
+                case 0:
+                    if (keyState.IsKeyDown(Keys.W))
+                    {
+                        _direction.Y--;
+                    }
+                    if (keyState.IsKeyDown(Keys.S))
+                    {
+                        _direction.Y++;
+                    }
+                    break;
+                case 1:
+                    if (keyState.IsKeyDown(Keys.Up))
+                    {
+                        _direction.Y--;
+                    }
+                    if (keyState.IsKeyDown(Keys.Down))
+                    {
+                        _direction.Y++;
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("test1");
+                    if (GamePad.GetState(PlayerIndex.One).DPad.Left == ButtonState.Pressed)
+                    {
+                        Console.WriteLine("test2");
+                        _direction.X--;
+                    }
+                    if (padOneState.DPad.Right == ButtonState.Pressed)
+                    {
+                        _direction.X++;
+                    }
+                    break;
+                case 3:
+                    if (padTwoState.DPad.Left == ButtonState.Pressed)
+                    {
+                        _direction.X--;
+                    }
+                    if (padTwoState.DPad.Right == ButtonState.Pressed)
+                    {
+                        _direction.X++;
+                    }
+                    break;
             }
+
             Movement(_direction, 500);
         }
-
     }
 }
