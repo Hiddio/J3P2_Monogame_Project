a8865c1cd57148b56634843e73d14169435899d7
Willemijn Sterken <0349974@student.rocvantwente.nl>
2023-11-21 11:15:43 +0100

    commented out errors

--- J3P2_Monogame_Project/J3P2_Monogame_Project/monoPong/MainMenu.cs
+++ J3P2_Monogame_Project/J3P2_Monogame_Project/monoPong/MainMenu.cs
@@ -20,8 +20,8 @@ namespace J3P2_Monogame_Project.monoPong
 
         public override void LoadContent(SceneManager sceneManager)
         {
-            PlayButton _playButton = new PlayButton(new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2, (_graphics.GraphicsDevice.Viewport.Height / 2) - 50), scale, _graphics, rectangle, sceneManager);
-            QuitButton _quitButton = new QuitButton(new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2, (_graphics.GraphicsDevice.Viewport.Height / 2) + 50), scale, _graphics, rectangle, sceneManager);
+            //PlayButton _playButton = new PlayButton(new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2, (_graphics.GraphicsDevice.Viewport.Height / 2) - 50), scale, _graphics, rectangle, sceneManager);
+            //QuitButton _quitButton = new QuitButton(new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2, (_graphics.GraphicsDevice.Viewport.Height / 2) + 50), scale, _graphics, rectangle, sceneManager);
 
             base.LoadContent(sceneManager);
         }
