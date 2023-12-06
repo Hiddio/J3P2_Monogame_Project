d2b53c883f622654030bd8caac5aa13c8f469b72
Hiddio <simon.verhoeff205@gmail.com>
2023-12-06 14:10:46 +0100

    Fixed Scale not being added onto the position

--- J3P2_Monogame_Project/J3P2_Monogame_Project/Framework/GameObject.cs
+++ J3P2_Monogame_Project/J3P2_Monogame_Project/Framework/GameObject.cs
@@ -11,8 +11,22 @@ namespace J3P2_Monogame_Project.Framework
         protected float _scale;
         protected GraphicsDevice _graphicsDevice;
         protected Rectangle? _hitbox;
-
-
+        public virtual Rectangle HitBox
+        {
+            // only has getter, it cannot be set to a value from anywhere
+            get
+            {
+                // if the backing field (our _hitbox rectangle) is null, then we set it to a new Rectangle with the correct scale.
+                _hitbox ??= new(0, 0, (int)(_texture.Width * _scale), (int)(_texture.Height * _scale));
+                // be cause the backingfield is nullable, we get the value cuz struct will be of type Nullable<Rectangle>
+                Rectangle rect = _hitbox.Value;
+                // set the position of the hitbox according to the objects position
+                rect.X = (int)_position.X - (_texture.Width * (int)_scale / 2);
+                rect.Y = (int)_position.Y - (_texture.Height * (int)_scale / 2);
+                // return the final hitbox
+                return rect;
+            }
+        }
         protected Texture2D debugTexture;
 
 
@@ -29,43 +43,19 @@ namespace J3P2_Monogame_Project.Framework
             _texture = pTexture;
         }
         
-        
-        public virtual Rectangle HitBox
-        {
-            // only has getter, it cannot be set to a value from anywhere
-            get
-            {
-                // if the backing field (our _hitbox rectangle) is null, then we set it to a new Rectangle with the correct scale.
-                _hitbox ??= new(0, 0, (int)(_texture.Width * _scale), (int)(_texture.Height * _scale));
-                // be cause the backingfield is nullable, we get the value cuz struct will be of type Nullable<Rectangle>
-                Rectangle rect = _hitbox.Value;
-                // set the position of the hitbox according to the objects position
-                rect.X = (int)_position.X - (_texture.Width / 2);
-                rect.Y = (int)_position.Y - (_texture.Height / 2);
-                // return the final hitbox
-                return rect;
-            }
-        }
         public virtual void Update(GameTime pGameTime)
         {   
+
         } 
         public virtual void Start()
         {
+
         }
         public virtual void Draw(SpriteBatch pSpriteBatch)
         {
-            pSpriteBatch.Draw(_texture, new Vector2(_position.X - (_texture.Width / 2), _position.Y - (_texture.Height / 2)), null, Color.White, 1, Vector2.Zero, _scale, SpriteEffects.None, 0);
+            pSpriteBatch.Draw(_texture, new Vector2(_position.X - (_texture.Width * _scale / 2), _position.Y - (_texture.Height * _scale / 2)), null, Color.White, 0, Vector2.Zero, _scale, SpriteEffects.None, 0);
         }
-        /// <summary>
-        /// Draw the rectangle.
-        /// </summary>
-        /// <param name="pSpriteBatch"></param>
-        public virtual void DrawRectangle(SpriteBatch pSpriteBatch)
-        {
-
-            // draw the rectangle
-            pSpriteBatch.Draw(debugTexture, new Rectangle((int)_position.X, (int)_position.Y, 1 * (int)_scale, 1 * (int)_scale), Color.White);
-        }
+        
         public virtual void DrawString(SpriteBatch pSpriteBatch)
         {
 
